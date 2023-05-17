ba97c754607fc9874319fdd5bfc2390d48e0663d
Holger Graef <holger.graef@gmail.com>
2023-05-18 00:27:38 +0200

    use MDI6 icons for BulbWidget

--- src/bulbwidget.cpp
+++ src/bulbwidget.cpp
@@ -3,10 +3,13 @@
 #include <QJsonObject>
 #include <QVBoxLayout>
 
+#include "main.h"
+
 BulbWidget::BulbWidget(const ordered_json& dev) {
     QVBoxLayout *layout = new QVBoxLayout();
 
     mBtnToggle.setText(QString::fromStdString(dev["name"]));
+    mBtnToggle.setIcon(awesome()->icon(fa::mdi6, fa::mdi6_lightbulb_outline));
 
     layout->addWidget(&mBtnToggle);
 
@@ -17,6 +20,6 @@ void BulbWidget::handleData(QString ip, QJsonDocument data) {
     (void) ip;
     if (data.isObject() && data.object().contains("is_on")) {
         bool is_on = data.object()["is_on"].toBool();
-        mBtnToggle.setText(is_on ? "on" : "off");
+        mBtnToggle.setIcon(awesome()->icon(fa::mdi6, is_on ? fa::mdi6_lightbulb_on : fa::mdi6_lightbulb_off));
     }
 }
