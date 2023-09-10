689583bc766e456ea509b1d4d90751abde81c7a0
Holger Graef <holger.graef@gmail.com>
2023-09-10 12:35:49 +0200

    Release v0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v0.1.2
+
+* improve slider behavior
+* improve lock screen behavior
+* show version on lock screen
--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 const std::string
 getVersionString()
