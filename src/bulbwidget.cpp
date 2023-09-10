c5b02dd8e267834f134b4f117e4d06408d9fcdf3
Holger Graef <holger.graef@gmail.com>
2023-09-10 14:26:23 +0200

    fix codeformat

--- src/bulbwidget.cpp
+++ src/bulbwidget.cpp
@@ -200,16 +200,15 @@ BulbWidget::setBrightness(int value)
 
   mDevIsBusy = true;
   mNextBrightnessValue = value;
-  int ret =
-    mDev->setBrightness(value * mDev->brightnessScale() / 100,
-                        [this](tuya::Device::CommandStatus status,
-                                      const ordered_json& data) {
-                          (void)status;
-                          (void)data;
-                          mDevIsBusy = false;
-                          if (status != tuya::Device::CMD_OK)
-                            mSetBrightnessTimer.start();
-                        });
+  int ret = mDev->setBrightness(
+    value * mDev->brightnessScale() / 100,
+    [this](tuya::Device::CommandStatus status, const ordered_json& data) {
+      (void)status;
+      (void)data;
+      mDevIsBusy = false;
+      if (status != tuya::Device::CMD_OK)
+        mSetBrightnessTimer.start();
+    });
   if (ret != 0)
     mSetBrightnessTimer.start();
 }
@@ -243,15 +242,15 @@ BulbWidget::setColorTemp(int value)
 
   mDevIsBusy = true;
   mNextColorTempValue = value;
-  int ret = mDev->setColorTemp(value * mDev->colorTempScale() / 100,
-                               [this](tuya::Device::CommandStatus status,
-                                             const ordered_json& data) {
-                                 (void)status;
-                                 (void)data;
-                                 mDevIsBusy = false;
-                                 if (status != tuya::Device::CMD_OK)
-                                   mSetColorTempTimer.start();
-                               });
+  int ret = mDev->setColorTemp(
+    value * mDev->colorTempScale() / 100,
+    [this](tuya::Device::CommandStatus status, const ordered_json& data) {
+      (void)status;
+      (void)data;
+      mDevIsBusy = false;
+      if (status != tuya::Device::CMD_OK)
+        mSetColorTempTimer.start();
+    });
   if (ret != 0)
     mSetColorTempTimer.start();
 }
